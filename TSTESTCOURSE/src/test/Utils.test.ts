adc2ccddc66f10dfc83db499262b211d363f08a0
GWilczyk <gwilczyk.github@gmail.com>
2021-02-26 23:10:08 +0100

    Project Setup: Jest describe and test

---
+++ TSTESTCOURSE/src/test/Utils.test.ts
@@ -0,0 +1,8 @@
+import { Utils } from '../app/Utils';
+
+describe('Utils Test Suite', () => {
+	test('First Test', () => {
+		const abc = Utils.toUpperCase('abc');
+		console.log('Tests work!!!');
+	});
+});
