e1a0212b4acd271d0d1b8ff8fc6a8d2590daeee1
GWilczyk <gwilczyk.github@gmail.com>
2021-02-27 00:08:46 +0100

    Assertions: expect toBe and toEqual

--- TSTESTCOURSE/src/test/Utils.test.ts
+++ TSTESTCOURSE/src/test/Utils.test.ts
@@ -2,7 +2,26 @@ import { Utils } from '../app/Utils';
 
 describe('Utils Test Suite', () => {
 	test('First Test', () => {
-		const abc = Utils.toUpperCase('abc');
-		console.log('Tests work!!!');
+		const result = Utils.toUpperCase('abc');
+		expect(result).toBe('ABC');
+	});
+
+	test('Parse Simple URL', () => {
+		const parsedUrl = Utils.parseUrl('http://localhost:8080/login');
+		expect(parsedUrl.href).toBe('http://localhost:8080/login');
+		expect(parsedUrl.port).toBe('8080');
+		expect(parsedUrl.protocol).toBe('http:');
+		expect(parsedUrl.query).toEqual({});
+	});
+
+	test('Parse URL with Query', () => {
+		const parsedUrl = Utils.parseUrl(
+			'http://localhost:8080/login?user=user&password=pass'
+		);
+		const expectedQuery = {
+			user: 'user',
+			password: 'pass'
+		};
+		expect(parsedUrl.query).toEqual(expectedQuery);
 	});
 });
